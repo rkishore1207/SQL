aeff8c3e43ad0ae9e27bf02e657f41cafae59098
rkishore1207 <rkishoreatr1207@gmail.com>
2024-08-30 18:10:58 +0530

    Duplicate Record Finding

--- Learnings.sql
+++ Learnings.sql
@@ -1,6 +1,10 @@
 
 -- Default Constraint
-
 ALTER TABLE config.Employee
 ADD CONSTRAINT DF_Employee_Gender
 DEFAULT 'Unknown' FOR emp_gender
+
+-- Find the Duplicate Records
+SELECT * FROM data.Product
+GROUP BY [Name]
+HAVING COUNT(*) > 1
