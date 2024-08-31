f8553596bd45b9f6338d8879a0fa1a2775bfa8de
rkishore1207 <rkishoreatr1207@gmail.com>
2024-08-31 13:02:58 +0530

    Cascading Referential Integrity

--- Learnings.sql
+++ Learnings.sql
@@ -8,3 +8,10 @@ DEFAULT 'Unknown' FOR emp_gender
 SELECT * FROM data.Product
 GROUP BY [Name]
 HAVING COUNT(*) > 1
+
+--Cascading Referential Integrity
+-- When one table is referenced with another table, there if we delete one record from Parent table we have to set some of the constraint to the
+-- respected child table
+
+-- 1. SET DEFAULT, 2. SET NULL, 3. CASCADING, 4. NO ACTION
+-- Open the child table go to the Relationship window, click the Insert and update section and select the required options for cascading.
